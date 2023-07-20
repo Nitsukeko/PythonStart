cb9c165aec512daf8079ed4883c64319822430f1
Nitsukeko <138388029+Nitsukeko@users.noreply.github.com>
2023-07-19 21:29:47 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+print("Hello! I am Nitsukeko")
+print('o----')
+print(' ||||')
+print('*' * 10)
+
+price = 10
+print(price)
+price = 20
+rating = 3.5
+name = 'Nitsukeko'
+is_cat = False
+print(price , rating , name , is_cat)
+
+Name = 'John Smith'
+age = '20 years old'
+status = 'new patient'
+print("We check in a patient named",Name, "He's", age, "and is a",status)
